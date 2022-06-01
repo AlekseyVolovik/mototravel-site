b7c56013a10329093cc31f03b5f5d104024f6e55
AlekseyVolovik <103026773+AlekseyVolovik@users.noreply.github.com>
2022-06-01 18:35:00 +0300

    add json-server for get form

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ window.onscroll = function() {
 
 window.scrollTo(0, 1);
 
-
 // Modal
 
 const modal = document.querySelector(".modal");
@@ -65,7 +64,197 @@ window.addEventListener("scroll", () => {
     }
 });
 
+//Forms get use XMLHttpRequest
+/*
+const forms = document.querySelectorAll('form');
 
-//Forms get
+const message = {
+    loading: "Loading...",
+    success: "Thanks, we'll call you back soon",
+    failure: "Error"
+};
+
+forms.forEach(item => {
+    postData(item);
+});
+
+function postData(form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const statusMessage = document.createElement("div");
+        statusMessage.classList.add('status');
+        statusMessage.textContent = message.loading;
+        statusMessage.style.cssText = `
+                text-align: center;
+            `;
+        form.insertAdjacentElement('afterend', statusMessage);
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+
+        request.setRequestHeader('Content-type', 'application/json');
+        const formData = new FormData(form);
+
+        const object = {};
+        formData.forEach(function(value, key) {
+            object[key] = value;
+        });
+
+        const json = JSON.stringify(object);
+
+        request.send(json);
+
+        request.addEventListener('load', () => {
+            if (request.status === 200) {
+                console.log(request.response);
+                statusMessage.textContent = message.success;
+                form.reset();
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 2000);
+            } else {
+                statusMessage.textContent = message.failure;
+                form.reset();
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 2000);
+            }
+        });
+    });
+}
+*/
+
+//Forms get use fetch
+/*
+const forms = document.querySelectorAll('form');
+
+const message = {
+    loading: "Loading...",
+    success: "Thanks, we'll call you back soon",
+    failure: "Error"
+};
+
+forms.forEach(item => {
+    postData(item);
+});
+
+function postData(form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const statusMessage = document.createElement("div");
+        statusMessage.classList.add('status');
+        statusMessage.textContent = message.loading;
+        statusMessage.style.cssText = `
+                text-align: center;
+            `;
+        form.insertAdjacentElement('afterend', statusMessage);
+
+        const formData = new FormData(form);
+
+        const object = {};
+        formData.forEach(function(value, key) {
+            object[key] = value;
+        });
+
+        fetch('server.php', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        })
+        .then(data => data.text())
+        .then(data => {
+            console.log(data);
+            statusMessage.textContent = message.success;
+        }).catch(() => {
+            statusMessage.textContent = message.failure;
+        }).finally(() => {
+            form.reset();
+            setTimeout(() => {
+                statusMessage.remove();
+            }, 2000);
+        })
+    });
+}
+*/
+
+//Forms get use json-server
+const forms = document.querySelectorAll('form');
+
+const message = {
+    loading: "Loading...",
+    success: "Thanks, we'll call you back soon",
+    failure: "Error"
+};
+
+forms.forEach(item => {
+    bindPostData(item);
+});
+
+const postData = async (url, data) => {
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: data
+    });
+
+    return await res.json();
+};
+
+function bindPostData(form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const statusMessage = document.createElement("div");
+        statusMessage.classList.add('status');
+        statusMessage.textContent = message.loading;
+        statusMessage.style.cssText = `
+                text-align: center;
+            `;
+        form.insertAdjacentElement('afterend', statusMessage);
+
+        const formData = new FormData(form);
+
+        const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+        
+
+        postData('http://localhost:3000/requests', json)
+        .then(data => {
+            console.log(data);
+            statusMessage.textContent = message.success;
+        }).catch(() => {
+            statusMessage.textContent = message.failure;
+        }).finally(() => {
+            form.reset();
+            setTimeout(() => {
+                statusMessage.remove();
+            }, 2000);
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
